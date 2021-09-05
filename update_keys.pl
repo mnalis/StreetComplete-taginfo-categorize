61a34be95dd7a03231a7c6014025745c392d26fc
Matija Nalis <mnalis-git@voyager.hr>
2021-09-05 23:07:49 +0200

    basic parsing of taginfo JSON data

---
+++ update_keys.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -T
+# by Matija Nalis <mnalis-git@voyager.hr> GPLv3+, started 20210905
+#
+# parse taginfo.openstreetmap.org output for a list of OSM keys like:
+# https://taginfo.openstreetmap.org/api/4/key/combinations?key=shop&filter=all&sortname=to_count&sortorder=desc&page=1&rp=501&qtype=other_key&format=json_pretty
+# and add any keys with usage > 0.01% and not already present in keys.txt into it.
+
+use strict;
+use warnings;
+use autodie qw/:all/;
+
+use JSON;
+
+# load existing keys
+my %existing = ();
+open my $existing_fd, '<', 'keys.txt';
+while (<$existing_fd>) {
+    next unless /^[[:alpha:]]/i;
+    $existing{$_} = $1;
+}
+
+# parse new keys
+
+open my $json_fd, '<', 'shop.json';
+local $/;
+my $json_all = (decode_json <$json_fd>)[0]->{'data'};
+my @json_filtered = grep { $_->{'to_fraction'} > 0.1 } @$json_all;
+my @tags_many = map { $_->{'other_key'} } @json_filtered;
+
+use Data::Dumper;
+print Dumper (\@tags_many);
